d7b71e78191cb5b53c2939b02cec637d430cd32d
Infidel <Vud150294@gmail.com>
2023-10-24 11:39:11 +0500

    в проект были добавлены встроенные во фреймворк аутентификации представления входа и выхода; • для обоих представлений были созданы конкретно-прикладные шаблоны и определено простое представление информационной панели, куда перенаправлять пользователей после их входа; • наконец, добавлены настроечные параметры, чтобы Django использовал эти URL-адреса по умолчанию.

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 
 
+@login_required
+# Декоратор login_required проверяет аутентификацию текущего пользователя.
+# Если пользователь аутентифицирован, то оно исполняет декорированное
+# представление; если пользователь не аутентифицирован, то оно перенаправляет
+# пользователя на URL-адрес входа с изначально запрошенным URL-адресом в качестве GET-параметра с именем next.
+def dashboard(request):
+    # Мы также определили переменную section.
+    # Эта переменная будет использоваться для подсвечивания текущего раздела в главном меню сайта.
+    return render(request,
+                  'account/dashboard.html',
+                  {'selection': 'dashboard'})
+
+
 def user_login(request):
+    #  authenticate() и login(): authenticate()
+    # проверяет учетные данные пользователя и возвращает объект User, если они
+    # правильны; login() задает пользователя в текущем сеансе.
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -15,11 +32,11 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated successfully')
+                    return HttpResponse('Аутентификация прошла успешно')
                 else:
-                    return HttpResponse('Disabled account')
+                    return HttpResponse('Отключенная учетная запись')
             else:
-                return HttpResponse('Invalid login')
+                return HttpResponse('Неверный логин или пароль')
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +122,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGIN_REDIRECT_URL: сообщает Django URL-адрес, на который следует перенаправлять
+# пользователя после успешного входа, если в запросе нет
+# параметра next;
+LOGIN_REDIRECT_URL = 'dashboard'
+
+# LOGIN_URL: URL-адрес, на который следует перенаправлять пользователя
+# чтобы зарегистрировать его вход (например, представления, в которых
+# используется декоратор login_required);
+LOGIN_URL = 'login'
+
+#  LOGOUT_URL: URL-адрес, на который следует перенаправлять пользователя, чтобы зарегистрировать его выход.
+LOGOUT_URL = 'logout'
