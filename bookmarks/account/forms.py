3ae9a246fb7f3b8c28739fd3bd7599adb2533e83
Infidel <Vud150294@gmail.com>
2023-10-25 11:47:40 +0500

    создана форма для до создания юзера

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
@@ -9,3 +10,23 @@ class LoginForm(forms.Form):
     # как ввод пароля.
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    # Здесь была создана модельная форма для модели пользователя. Данная
+    # форма содержит поля username, first_name и email модели User. Указанные
+    # поля будут валидироваться в соответствии с проверками на валидность соответствующих полей модели
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password']
