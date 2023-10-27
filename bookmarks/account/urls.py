de7a359298d83024345661ead4f506cf9008e289
Infidel <Vud150294@gmail.com>
2023-10-27 09:52:17 +0500

    удалено название

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     #      auth_views.PasswordResetCompleteView.as_view(),
     #      name='password_reset_complete'),
 
-    path('', include("django.contrib.auth.urls"), name='dashboard'),
+    path('', include("django.contrib.auth.urls")),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
