9d953af6a65e83d5cc7de95cbaffe38c06ddd248
Infidel <Vud150294@gmail.com>
2023-10-31 15:43:54 +0500

    исправлено неверное название переменной

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Image(models.Model):
@@ -31,7 +32,7 @@ class Image(models.Model):
     slug = models.SlugField(max_length=200,
                             blank=True)
     url = models.URLField(max_length=2000)
-    image = models.ImageField(upload_to='images/%/%m/%d')
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     # В данном случае понадобится взаимосвязь многие-ко-многим, поскольку пользователю может
@@ -39,9 +40,9 @@ class Image(models.Model):
     # нескольким пользователям.
     # При определении поля ManyToManyField Django создает промежуточную таблицу соединения,
     # используя первичные ключи обеих моделей.
-    user_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                       related_name='images_liked',
-                                       blank=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
 
     class Meta:
         # Индексы базы данных повышают производительность запросов.
@@ -63,6 +64,11 @@ class Image(models.Model):
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
+    def get_absolute_url(self):
+        # общепринятым способом предоставления канонических
+        # URL-адресов объектам является определение метода get_absolute_url() в модели
+        return reverse('images:detail', args=[self.id,
+                                              self.slug])
+
     def __str__(self):
         return self.title
-
