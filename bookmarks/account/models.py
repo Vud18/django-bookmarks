9c1e04cce9bbdb774beb6fb2a558558524a34c56
Infidel <Vud150294@gmail.com>
2023-11-01 15:56:54 +0500

    добавлена модель Contact для взаимосвязи пользователей. Добавлено поле в модель User "following"

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
@@ -15,3 +16,48 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile of {self.user.username}'
+
+
+class Contact(models.Model):
+    """
+    В приведенном выше исходном коде показана модель Contact, которая
+        будет использоваться для взаимосвязей пользователей. Она содержит следующие поля:
+        • user_from: внешний ключ (ForeignKey) для пользователя, который создает взаимосвязь;
+        • user_to: внешний ключ (ForeignKey) для пользователя, на которого есть
+        подписка;
+        • created: поле DateTimeField с параметром auto_now_add=True для хранения
+        времени создания взаимосвязи.
+        Для полей ForeignKey индекс базы данных создается автоматически.
+         В Metaклассе модели такой индекс определен в убывающем порядке по полю created.
+        Также был добавлен атрибут ordering, чтобы сообщать Django
+        , что по умолчанию он должен сортировать результаты по полю created. Используя дефис
+    перед именем поля, указывается убывающий порядок. Например, -сreated.
+    """
+    user_form = models.ForeignKey('auth.User',
+                                  related_name='rel_from_set',
+                                  on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User',
+                                related_name='rel_to_set',
+                                on_delete=models.CASCADE)
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_form} follows {self.user_to}'
+
+
+# добавить следующее поле в User динамически
+# Здесь модель User извлекается встроенной в  Django типовой функцией
+# get_user_model(). Метод add_to_class() моделей Django применяется для того,
+# чтобы динамически подправлять модель User
+user_model = get_user_model()
+user_model.add_to_class('following',
+                        models.ManyToManyField('self',
+                                               through=Contact,
+                                               related_name='followers',
+                                               symmetrical=False))
