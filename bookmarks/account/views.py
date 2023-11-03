f00709a4a9c0994ca71b75757f1409205825d5ea
Infidel <Vud150294@gmail.com>
2023-11-03 13:30:53 +0500

    добавлено представление для отслеживания фолловеров

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
 
 
 @login_required
@@ -134,3 +135,24 @@ def user_detail(request, username):
                   'account/user/detail.html',
                   {'section': 'people',
                    'user': user})
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_form=request.user,
+                    user_to=user)
+            else:
+                Contact.objects.filter(user_form=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
